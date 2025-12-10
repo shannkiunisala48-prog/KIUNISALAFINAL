bd34e356f1dcf49003c62420c6435ddd2adbea18
shannkiunisala48-prog <shannkiunisala48@gmail.com>
2025-12-11 07:58:03 +0800

    Update code20.java

--- code20.java
+++ code20.java
@@ -1,22 +1,10 @@
 
-/**
- * Write a description of class code20 here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
 public class code20
 {
      public static void main(String[] args) {
 
         int[] numbers = { 10, 25, 30, 15, 40, 18, 50,60,29,70 };
-
-        System.out.print("Array: ");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-        }
-        System.out.println();
-
+        
         System.out.print("Even numbers greater than 20: ");
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > 20 && numbers[i] % 2 == 0) {
